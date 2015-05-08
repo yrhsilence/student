a1c49ded583e5b36eea328e24cb5d34b79da59f1
yrhsilence <yrhsilence@gmail.com>
2015-05-08 17:41:15 +0800

    add java

---
+++ javalearn/src/main/java/cn/vobile/javalearn/threadpool/MyThread.java
@@ -0,0 +1,11 @@
+package cn.vobile.javalearn.threadpool;
+
+/**
+ * Created by vobile on 5/7/15.
+ */
+public class MyThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + "is running...");
+    }
+}
