3281d1176af26cb20430be079f4021f96323fcbe
yrhsilence <yrhsilence@gmail.com>
2015-06-16 15:10:57 +0800

    add LoveFinder

--- swift/LoveFinder/LoveFinder/ViewController.swift
+++ swift/LoveFinder/LoveFinder/ViewController.swift
@@ -8,18 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var name: UITextField!
+    
+    @IBOutlet weak var gender: UISegmentedControl!
+    
+    @IBOutlet weak var birthday: UIDatePicker!
+    
+    @IBOutlet weak var heightNumber: UISlider!
+    
+    @IBOutlet weak var height: UILabel!
+    
+    @IBOutlet weak var hasProperty: UISwitch!
+    
+
+    @IBOutlet weak var result: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        name.delegate = self
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func heightChanged(sender: AnyObject) {
+        var slider = sender as! UISlider
+        var i = Int(slider.value)
+        slider.value = Float(i)
+        height.text = "\(i)cm"
+    }
 
+    
+    @IBAction func okTapped(sender: AnyObject) {
+        let genderText = gender.selectedSegmentIndex == 0 ? "高富帅": "白富美"
+        
+        let gregorian = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)
+        let now = NSDate()
+        let components = gregorian!.components(NSCalendarUnit.YearCalendarUnit, fromDate: birthday.date, toDate: now, options: NSCalendarOptions(0))
+        let age = components.year
+        
+        let hasPropertyText = hasProperty.on ? "有房": "没房"
+        
+        result.text = "\(name.text), \(age)岁, \(genderText), 身高\(Int(heightNumber.value))cm, \(hasPropertyText), 求包养"
+    }
+    
+    // UITextFiledDeleate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
 }
 
